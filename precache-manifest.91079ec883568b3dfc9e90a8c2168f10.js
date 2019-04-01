5465f86b10fad6b2c506c893b3b47ff9074fceee
vlanemcev <vlanemcev@gmail.com>
2019-04-01 17:05:07 +0300

    Updates

---
+++ precache-manifest.91079ec883568b3dfc9e90a8c2168f10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ccb90dd3a411b3b78f",
+    "url": "/task-timer/static/js/runtime~main.546b7746.js"
+  },
+  {
+    "revision": "f7edbb4c62561e23dae1",
+    "url": "/task-timer/static/js/main.88e37c86.chunk.js"
+  },
+  {
+    "revision": "c42faf4ca2c9546f0f3d",
+    "url": "/task-timer/static/js/2.41dfa8d5.chunk.js"
+  },
+  {
+    "revision": "f7edbb4c62561e23dae1",
+    "url": "/task-timer/static/css/main.813d7c23.chunk.css"
+  },
+  {
+    "revision": "c1133b5b73cf1364bbc2e6fbe5612c61",
+    "url": "/task-timer/index.html"
+  }
+];
