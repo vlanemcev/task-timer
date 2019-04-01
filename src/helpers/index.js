6a89daebf10c4e190a22fd7e3bda3cc7751132b3
vlanemcev <vlanemcev@gmail.com>
2019-04-01 16:29:49 +0300

    Added a parameter to the time formatting function - show milliseconds or not. Now, time is displayed depending on this parameter.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
-export const formatTime = (time, hrsInLocal) => {
+export const formatTime = (time, hrsInLocal, showMSec = false) => {
+  const cutMsec = Math.floor((time % 1000) / 100);
   const sec = time / 1000;
   const cutSec = Math.floor(sec % 60);
   const min = Math.floor((sec % 3600) / 60);
@@ -8,7 +9,14 @@ export const formatTime = (time, hrsInLocal) => {
   );
 
   // prettier-ignore
-  return hrsInLocal ?
-    `${hrsLoc < 10 ? "0" : ""}${hrsLoc}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}` :
-    `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}`;
+  if (hrsInLocal) {
+    return showMSec
+      ? `${hrsLoc < 10 ? "0" : ""}${hrsLoc}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}:${cutMsec}`
+      : `${hrsLoc < 10 ? "0" : ""}${hrsLoc}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}`;
+  }
+
+  // prettier-ignore
+  return showMSec
+    ? `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}:${cutMsec}`
+    : `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}`;
 };
