25070c5a989a1503a284bb3a7b916b6fdabcd797
vlanemcev <vlanemcev@gmail.com>
2019-04-01 15:43:48 +0300

    Сonnected react-redux and react-router.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages/App/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "./domain/store";
+import App from "./pages/App/index";
 
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
 
-import './index.css';
+import * as serviceWorker from "./serviceWorker";
+
+const store = configureStore();
 
 ReactDOM.render(
-		<App />,
-		document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
