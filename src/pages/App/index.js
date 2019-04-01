5351087a2c6427daf023ae7d931906fa719c4b10
vlanemcev <vlanemcev@gmail.com>
2019-04-01 15:10:25 +0300

    Defined the routes for the application, transferred the logic of displaying the main page.

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,48 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router";
 
-import Input from "../../components/Input/index";
-import Timer from "../../components/Timer/index";
+import Home from "pages/Home/index";
+import Task from "pages/Task/index";
+import NoMatch from "pages/NoMatch/index";
 
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import styles from "./styles";
 
 class App extends Component {
-	render() {
-		const { classes } = this.props;
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
 
-		return (
-				<React.Fragment>
-					<CssBaseline />
-					<div className={classes.layout}>
-						<Paper>
-							<Grid container spacing={32} justify={"center"}>
-								<Grid
-										item
-										xs={12}
-										classes={{item: classes.taskInputWrap}}
-								>
-									<Input
-											placeholder={"Name of your Task"}
-											classes={{input: classes.taskNameInput}}
-									/>
-								</Grid>
-								<Grid
-										item
-										xs={12}
-										classes={{item: classes.timerWrap}}
-								>
-									<Timer />
-								</Grid>
-							</Grid>
-						</Paper>
-					</div>
-				</React.Fragment>
-		);
-	}
+        <Switch>
+          <Route exact path={"/"} render={() => <Redirect to={"/tasks"} />} />
+          <Route exact path={"/tasks/:id"} component={Task} />
+          <Route exact path={"/tasks"} component={Home} />
+          <Route exact path={"/chart"} component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
