1dabf6fb3388237216f4f38963d73ec1470a86c7
vlanemcev <vlanemcev@gmail.com>
2019-04-01 14:48:38 +0300

    The timer has become more scalable and flexible. In the timer stop handler, callback calls from parent component - are implemented. Now the parent component can control the timer after the stop action.

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,78 +1,169 @@
-import React, {Component} from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { formatTime } from "../../helpers";
+
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 import styles from "./styles.module.scss";
 
 class Timer extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			timerOn: false,
-			startTime: 0,
-			time: 0
-		};
+    this.state = {
+      timerOn: false,
+      isPause: false,
+      isResumed: false,
+      startTime: 0,
+      time: 0
+    };
 
-		this.timerID = null;
-	}
+    this.timerID = null;
+  }
 
-	startTimer = () => {
-		this.setState({
-			timerOn: true,
-			startTime: Date.now() - this.state.time,
-			time: this.state.time
-		});
+  startTimer = (resumed) => {
+    this.setState({
+      timerOn: true,
+      isResumed: !!resumed,
+      startTime: Date.now() - this.state.time,
+      time: this.state.time
+    });
 
-		this.timerID = setInterval(() => {
-			this.setState({
-				time: Date.now() - this.state.startTime
-			})
-		},1000);
-	}
+    this.timerID = setInterval(() => {
+      this.setState({
+        time: Date.now() - this.state.startTime
+      });
+    }, 1000);
+  };
 
-	stopTimer = () => {
-		this.resetTimer();
-		clearInterval(this.timerID);
-	}
+  pauseTimer = () => {
+    this.setState({
+      timerOn: false,
+      isPause: true
+    });
 
-	resetTimer = () => {
-		this.setState({
-			timerOn: false,
-			time: 0,
-			startTime: 0
-		});
-	}
+    clearInterval(this.timerID);
+  };
 
-	formatTime = (time) => {
-		const sec = time / 1000;
-		const cutSec = Math.floor(sec % 60);
-		const min = Math.floor((sec % 3600 ) / 60);
-		const hrs = Math.floor(sec / 3600);
+  resumeTimer = () => {
+    this.startTimer(true);
+  };
 
-		return `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}`;
-	}
+  stopTimer = () => {
+    const { time, startTime } = this.state;
+    const { onTimerStop, afterStopAction } = this.props;
 
-	render() {
-		const { time, timerOn } = this.state;
+    this.pauseTimer();
 
-		return (
-				<Grid container spacing={16}>
-					<Grid item xs={12}>
-						<div className={styles.timerWrap}>
-							{this.formatTime(time)}
-						</div>
-					</Grid>
-					<Grid item xs={12}>
-						{
-							timerOn ?
-									<Button variant="contained" onClick={this.stopTimer}>Stop</Button> :
-									<Button variant="contained" onClick={this.startTimer}>Start</Button>
-						}
-					</Grid>
-				</Grid>
-		);
-	}
+    if (typeof onTimerStop === "function") {
+      onTimerStop(
+        {
+          id: this.timerID,
+          startTime,
+          endTime: time + startTime
+        },
+        this[`${afterStopAction}Timer`]
+      );
+    } else {
+      this[`${afterStopAction}Timer`]();
+    }
+  };
+
+  resetTimer = () => {
+    clearInterval(this.timerID);
+
+    this.setState({
+      timerOn: false,
+      isPause: false,
+      isResumed: false,
+      time: 0,
+      startTime: 0
+    });
+  };
+
+  render() {
+    const { time, timerOn, isPause, isResumed } = this.state;
+    const { stopBtnText } = this.props;
+
+    return (
+      <Grid container spacing={16}>
+        <Grid item xs={12}>
+          <div className={styles.timerWrap}>{formatTime(time)}</div>
+        </Grid>
+        <Grid item xs={12}>
+          {timerOn ? (
+            isResumed ? (
+              <Grid container justify={"center"} spacing={16}>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color={"secondary"}
+                    onClick={this.stopTimer}
+                  >
+                    {stopBtnText}
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant={"contained"} onClick={this.resetTimer}>
+                    Reset timer
+                  </Button>
+                </Grid>
+              </Grid>
+            ) : (
+              <Button
+                variant="contained"
+                color={"secondary"}
+                onClick={this.stopTimer}
+              >
+                {stopBtnText}
+              </Button>
+            )
+          ) : isPause ? (
+            <Grid container justify={"center"} spacing={16}>
+              <Grid item>
+                <Button
+                  variant={"contained"}
+                  color={"secondary"}
+                  onClick={this.stopTimer}
+                >
+                  {stopBtnText}
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant={"contained"}
+                  color={"primary"}
+                  onClick={this.resumeTimer}
+                >
+                  Resume
+                </Button>
+              </Grid>
+            </Grid>
+          ) : (
+            <Button
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.startTimer(false)}
+            >
+              Start
+            </Button>
+          )}
+        </Grid>
+      </Grid>
+    );
+  }
 }
+
+Timer.propTypes = {
+  stopBtnText: PropTypes.string,
+  onTimerStop: PropTypes.func,
+  afterStopAction: PropTypes.oneOf(["resume", "reset", "pause"])
+};
+
+Timer.defaultProps = {
+  stopBtnText: "Stop",
+  afterStopAction: "reset"
+};
 
 export default Timer;
