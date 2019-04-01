4705ebf517ae3dca9496d831e8b54a5bb5ff8b24
vlanemcev <vlanemcev@gmail.com>
2019-04-01 15:08:23 +0300

    Layout for component to display task chart.

---
+++ src/components/TaskChart/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+
+class TaskChart extends Component {
+  render() {
+    return (
+      <div>
+        <Typography align={"center"} color={"textPrimary"}>
+          Hello, i`m chart component
+        </Typography>
+      </div>
+    );
+  }
+}
+
+export default TaskChart;
