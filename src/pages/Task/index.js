caf841aec7eba43d75152b6613691669914c32cd
vlanemcev <vlanemcev@gmail.com>
2019-04-02 09:21:28 +0300

    Removed the redirect to the "not found" page when switching to a nonexistent task. Now the error is displayed inside the page with the task.

--- src/pages/Task/index.js
+++ src/pages/Task/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { compose } from "redux";
 import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { formatTime } from "helpers/index.js";
 
 import { withStyles } from "@material-ui/core/styles";
@@ -18,57 +18,67 @@ class Task extends Component {
   render() {
     const { match, task, classes } = this.props;
 
-    return task["id"] ? (
+    return (
       <div className={classes.layout}>
         <Paper className={classes.paper}>
-          <Grid
-            container
-            spacing={32}
-            alignItems={"baseline"}
-            justify={"center"}
-          >
-            <Grid item xs={6}>
-              <Typography variant="h4" align="left">
-                {task["name"]}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h4" align="right">
-                {task["id"]}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider className={classes.divider} />
-          <Grid container spacing={32} justify={"center"}>
-            <Grid item xs={6}>
-              <Typography variant="subtitle1">Task start time</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="subtitle2" align="right">
-                {formatTime(task["startTime"], true)}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={32} justify={"center"}>
-            <Grid item xs={6}>
-              <Typography variant="subtitle1">Task end time</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="subtitle2" align="right">
-                {formatTime(task["endTime"], true)}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={32} justify={"center"}>
-            <Grid item xs={6}>
-              <Typography variant="subtitle1">Spend Time</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h5" align="right">
-                {formatTime(task["spendTime"])}
-              </Typography>
-            </Grid>
-          </Grid>
+          {task["id"] ? (
+            <React.Fragment>
+              <Grid
+                container
+                spacing={32}
+                alignItems={"baseline"}
+                justify={"center"}
+              >
+                <Grid item xs={6}>
+                  <Typography variant="h4" align="left">
+                    {task["name"]}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="h4" align="right">
+                    {task["id"]}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Divider className={classes.divider} />
+              <Grid container spacing={32} justify={"center"}>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle1">Task start time</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle2" align="right">
+                    {formatTime(task["startTime"], true)}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={32} justify={"center"}>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle1">Task end time</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle2" align="right">
+                    {formatTime(task["endTime"], true)}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={32} justify={"center"}>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle1">Spend Time</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="h5" align="right">
+                    {formatTime(task["spendTime"])}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </React.Fragment>
+          ) : (
+            <Typography variant="h5" align="right">
+              {`Sorry, but task with ID: ${
+                match.params["id"]
+              } - does not exist`}
+            </Typography>
+          )}
           <div className={classes.homeBtn}>
             <Button
               variant={"contained"}
@@ -81,17 +91,6 @@ class Task extends Component {
           </div>
         </Paper>
       </div>
-    ) : (
-      <Redirect
-        to={{
-          pathname: "/task-not-found",
-          state: {
-            errorText: `Sorry, but task with ID: ${
-              match.params["id"]
-            } - does not exist`
-          }
-        }}
-      />
     );
   }
 }
