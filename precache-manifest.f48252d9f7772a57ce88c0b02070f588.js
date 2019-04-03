fbeb21e3150d21400d0b002f4cc7c59782197d11
vlanemcev <vlanemcev@gmail.com>
2019-04-03 09:17:12 +0300

    Updates

---
+++ precache-manifest.f48252d9f7772a57ce88c0b02070f588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ccb90dd3a411b3b78f",
+    "url": "/task-timer/static/js/runtime~main.546b7746.js"
+  },
+  {
+    "revision": "e182767cb08a7f32ba6d",
+    "url": "/task-timer/static/js/main.3f36c332.chunk.js"
+  },
+  {
+    "revision": "946cdaa9d21f596eb530",
+    "url": "/task-timer/static/js/2.c6152873.chunk.js"
+  },
+  {
+    "revision": "e182767cb08a7f32ba6d",
+    "url": "/task-timer/static/css/main.813d7c23.chunk.css"
+  },
+  {
+    "revision": "b5198985875fc5fd500af2b1dddcee39",
+    "url": "/task-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/task-timer/precache-manifest.d1f085efb054b403abeaffe8761c557d.js"
+  "/task-timer/precache-manifest.f48252d9f7772a57ce88c0b02070f588.js"
 );
 
 workbox.clientsClaim();
