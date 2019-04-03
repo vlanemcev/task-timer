f3b5c79a8d3feb6ffc46a5324ab6f222ad3dd597
vlanemcev <vlanemcev@gmail.com>
2019-04-03 09:08:43 +0300

    Created Tasks Chart component.

--- src/components/TaskChart/index.js
+++ src/components/TaskChart/index.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
-import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  ResponsiveContainer,
+  BarChart,
+  CartesianGrid,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend
+} from "recharts";
+
+import { tasksHoursPartitioning } from "helpers";
 
 class TaskChart extends Component {
   render() {
+    const { tasks } = this.props;
+
     return (
-      <div>
-        <Typography align={"center"} color={"textPrimary"}>
-          Hello, i`m chart component
-        </Typography>
-      </div>
+      <ResponsiveContainer height={300}>
+        <BarChart
+          data={tasksHoursPartitioning(tasks)}
+          margin={{ top: 30, right: 40, left: 10, bottom: 10 }}
+        >
+          <CartesianGrid strokeDasharray={"3 3"} />
+          <XAxis dateKey={"hour"} />
+          <YAxis />
+          <Legend />
+          <Bar
+            dataKey={"occupied"}
+            name={"Minutes in this hours"}
+            fill={"#8884d8"}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     );
   }
 }
 
-export default TaskChart;
+TaskChart.propTypes = {
+  tasks: PropTypes.array.isRequired
+};
+
+export default connect((state) => {
+  return {
+    tasks: state.tasks
+  };
+})(TaskChart);
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -20,3 +20,73 @@ export const formatTime = (time, hrsInLocal, showMSec = false) => {
     ? `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}:${cutMsec}`
     : `${hrs < 10 ? "0" : ""}${hrs}:${min < 10 ? "0" : ""}${min}:${cutSec < 10 ? "0" : ""}${cutSec}`;
 };
+
+export const tasksHoursPartitioning = (tasks = []) => {
+  let tasksPartitioningStructure = [
+    { hour: 0, occupied: 0 },
+    { hour: 1, occupied: 0 },
+    { hour: 2, occupied: 0 },
+    { hour: 3, occupied: 0 },
+    { hour: 4, occupied: 0 },
+    { hour: 5, occupied: 0 },
+    { hour: 6, occupied: 0 },
+    { hour: 7, occupied: 0 },
+    { hour: 8, occupied: 0 },
+    { hour: 9, occupied: 0 },
+    { hour: 10, occupied: 0 },
+    { hour: 11, occupied: 0 },
+    { hour: 12, occupied: 0 },
+    { hour: 13, occupied: 0 },
+    { hour: 14, occupied: 0 },
+    { hour: 15, occupied: 0 },
+    { hour: 16, occupied: 0 },
+    { hour: 17, occupied: 0 },
+    { hour: 18, occupied: 0 },
+    { hour: 19, occupied: 0 },
+    { hour: 20, occupied: 0 },
+    { hour: 21, occupied: 0 },
+    { hour: 22, occupied: 0 },
+    { hour: 23, occupied: 0 }
+  ];
+
+  tasks.forEach((task) => {
+    const taskStartDate = new Date(task.startTime);
+    const taskStartHours = taskStartDate.getHours();
+    const taskEndDate = new Date(task.endTime);
+    const taskEndHours = taskEndDate.getHours();
+    const timeDifferenceHours = taskEndHours - taskStartHours;
+
+    if (timeDifferenceHours === 0) {
+      let partitioningElem = tasksPartitioningStructure.find(
+        (item) => item.hour === taskStartHours
+      );
+
+      partitioningElem.occupied =
+        partitioningElem.occupied + Math.floor(task.spendTime / 1000 / 60);
+    } else {
+      for (let i = taskStartHours; i <= taskEndHours; i++) {
+        let partitioningElem = tasksPartitioningStructure.find(
+          (item) => item.hour === i
+        );
+
+        switch (i) {
+          case taskStartHours:
+            partitioningElem.occupied =
+              partitioningElem.occupied + (60 - taskStartDate.getMinutes());
+            break;
+
+          case taskEndHours:
+            partitioningElem.occupied =
+              partitioningElem.occupied + taskEndDate.getMinutes();
+            break;
+
+          default:
+            partitioningElem.occupied = 60;
+            break;
+        }
+      }
+    }
+  });
+
+  return tasksPartitioningStructure;
+};
